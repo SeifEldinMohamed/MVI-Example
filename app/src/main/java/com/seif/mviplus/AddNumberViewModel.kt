eaabd7fcc97e4c7191fdb5c81c2481fe1052b39f
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-07-17 16:59:41 +0200

    Part 2

--- app/src/main/java/com/seif/mviplus/AddNumberViewModel.kt
+++ app/src/main/java/com/seif/mviplus/AddNumberViewModel.kt
@@ -1,15 +1,48 @@
 package com.seif.mviplus
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 
-class AddNumberViewModel: ViewModel() {
+class AddNumberViewModel : ViewModel() {
+    // we need to open channel bet viewModel and activity to send those intents
+    val intentChannel = Channel<MainIntent>(Channel.UNLIMITED)
+
+    // encapsulation
+    private val _viewState = MutableStateFlow<MainViewState>(MainViewState.Idle)
+    val state: StateFlow<MainViewState> get() = _viewState
+    private var number = 0
+
+    init {
+        processIntent()
+    }
     // process intent
-    private fun processIntent(){
+    private fun processIntent() { // we can call it from mainActivity if it's public
+        viewModelScope.launch {
+            intentChannel.consumeAsFlow().collect {
+                when (it) {
+                    is MainIntent.AddNumber -> addNumber()
+                }
+            }
+        }
 
     }
-    // reduce result came from process to send it to activity
-    private fun reduceResult(){
 
+    // reduce result came from process to send it to activity
+    private fun addNumber() {
+        viewModelScope.launch {
+            _viewState.value =
+                try {
+                    MainViewState.Number(number++)
+                } catch (e: Exception) {
+                    MainViewState.Error(e.message!!)
+                }
+        }
     }
 }
--- app/src/main/java/com/seif/mviplus/MainActivity.kt
+++ app/src/main/java/com/seif/mviplus/MainActivity.kt
@@ -2,24 +2,38 @@ package com.seif.mviplus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.seif.mviplus.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private val viewModel: AddNumberViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        render()
         binding.btnAddNumber.setOnClickListener {
             // send intent
-
+            lifecycleScope.launch {
+                viewModel.intentChannel.send(MainIntent.AddNumber)
+            }
         }
-
-
     }
-    private fun render() {
-        // render result
 
+    private fun render() { // observe on this data
+        // render result we need to collect data came from stateFlow coming from viewModel
+        lifecycleScope.launchWhenStarted {
+            viewModel.state.collect {
+                when(it){
+                    is MainViewState.Idle -> binding.txtNumber.text = getString(R.string.idle)
+                    is MainViewState.Number -> binding.txtNumber.text = it.number.toString()
+                    is MainViewState.Error -> binding.txtNumber.text = it.error
+                }
+            }
+        }
     }
 }
